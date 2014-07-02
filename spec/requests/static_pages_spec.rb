84c0c2de952a75b0f8c87a802f5b27b0290e5560
Greg Stanring <standringg@gmail.com>
2014-07-02 15:46:49 +1200

    Finished static pages, included route for heroku functionality

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  describe "Home page" do
+  	
+    it "it should have the content 'Sample App'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+    it "should have the right title'Home'" do
+ 	 visit '/static_pages/home'
+ 	 expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+	end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the right title'Help'" do
+ 	 visit '/static_pages/help'
+ 	 expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+	end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the right title'About Us'" do
+ 	 visit '/static_pages/about'
+ 	 expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+	end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the right title'Contact'" do
+ 	 visit '/static_pages/contact'
+ 	 expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+	end
+  end
+end
